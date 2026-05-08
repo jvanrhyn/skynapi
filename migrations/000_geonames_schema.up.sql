e382a0a74c36ab96fee107c9857806510909b0ba
Johan van Rhyn <github@jvanrhyn.co.za>
2026-05-08 20:36:16 +0200

    fix: update Dockerfile to handle Git LFS pointers in migrations and enable LFS in workflow

--- migrations/000_geonames_schema.up.sql
+++ migrations/000_geonames_schema.up.sql
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:eee06b909791e7f97be2870cab027a25f367c9c17f35f1e6f1ec375d1ec53c55
-size 147162908
+oid sha256:f5684900b8c5001cc5b3948c0ab2dd054b1b848e38fd6938f79171650ea7cdb9
+size 147162944
